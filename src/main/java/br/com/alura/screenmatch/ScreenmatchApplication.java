85a70bb72780a14d105dcce82476c3f859b1fbdf
RuanAlmeida <Ruan.aalmeidasantos@gmail.com>
2026-04-16 21:02:46 -0300

    consumindo api

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.serivce.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("hello world!");
+		var ConsumoApi = new ConsumoApi();
+	//	var json = ConsumoApi
+	//				.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=66106cea");
+	//	System.out.println(json);
 	}
 }
